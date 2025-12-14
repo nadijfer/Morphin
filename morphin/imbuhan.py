2cbb40a3878dd3b32cdec8b0b38f61b3027f19b2
Nadhif Adita <nadijfer02@gmail.com>
2025-12-14 09:45:09 +0700

    fix perbaikan imbuhan

--- morphin/imbuhan.py
+++ morphin/imbuhan.py
@@ -3,18 +3,23 @@
 1.1. metode periksa aturan
     1. Identifikasi letak awalan me
     2. Periksa peleburan me-:
-        1. meng-    jika a, i, u, e, o, g, h, dan kh
-        2. men-     jika c, d, j
-        3. mem-     jika b, f, v
+        1. meng-    jika a, i, u, e, o, g, h, dan kh, k
+        2. men-     jika c, d, j, t
+        3. mem-     jika b, f, v, p
         4. me-      jika r, l, w, y, m, n, ng, dan ny
     3. periksa kata dasar setelah awalan yang salah
     4. perbaiki sesuai poin no. 2
+1.2. metode window-based
+    1. stem kata
 '''
 
-def imbuhan_awalan(kata):
+def perbaiki_awalan(kata):
+    fixed = kata
     if kata.startswith('meng'):
         if kata[4] in ('a', 'i', 'u', 'e', 'o', 'g', 'h', 'kh'):
             fixed = kata
+        elif kata[4] in 'k':
+            fixed = 'meng'+kata[5:]
         else:
             # print('DARI MENG-')
             if kata[4] in ('c', 'd', 'j'):
@@ -26,6 +31,10 @@ def imbuhan_awalan(kata):
     elif kata.startswith('men'):
         if kata[3] in ('c', 'd', 'j'):
             fixed = kata
+        elif kata[3] in 't':
+            fixed = 'men'+kata[4:]
+        elif kata[3] in 's':
+            fixed = 'meny'+kata[4:]
         else:
             # print('DARI MEN-')
             if kata[3] in ('b', 'f', 'v'):
@@ -37,6 +46,10 @@ def imbuhan_awalan(kata):
     elif kata.startswith('mem'):
         if kata[3] in ('b', 'f', 'v'):
             fixed = kata
+        elif kata[3] in 'pr':
+            fixed = 'mem'+kata[4:]
+        elif kata[3] in 'p':
+            fixed = 'mem'+kata[4:]
         else:
             # print('DARI MEM-')
             if kata[3] in ('c', 'd', 'j'):
@@ -56,4 +69,22 @@ def imbuhan_awalan(kata):
                 fixed = 'me' + kata[2:]
             elif kata[2] in ('a', 'i', 'u', 'e', 'o', 'g', 'h', 'kh'):
                 fixed = 'meng' + kata[2:]
+    return fixed
+
+def perbaiki_nya(kata):
+    fixed_list = kata.split()
+    for i in range(len(fixed_list)):
+        if fixed_list[i] == 'nya':
+            fixed = fixed_list[i-1].join
+            fixed = "".join(fixed_list)
+    return fixed
+
+def perbaiki_kalimat(kalimat):
+    print(f'kalimat salah: {kalimat}')
+    pecah = kalimat.split()
+    for i in range(len(pecah)):
+        if pecah[i].startswith('me'):
+            pecah[i] = perbaiki_awalan(pecah[i])
+            print(f'perbaikan (list): {pecah}, kata: {pecah[i]}')
+    fixed = " ".join(pecah)
     return fixed
